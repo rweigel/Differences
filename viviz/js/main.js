716905736970f9ef01894be1c93939b91ddabbf5
Bob Weigel <rweigel@gmu.edu>
2014-10-30 16:16:09 -0400

    .gitignore and viviz

---
+++ viviz/js/main.js
@@ -0,0 +1,63 @@
+// TODO: $.gallery({"showdropdowns":true,"showfilmstrip":true,"showattributes":true,"showconfiguration":true,"showprint",true,"showgrid",true,"showabout":true,"compact","false"]})
+// TODO:
+//     If vector image, use imgconvert.org.
+//     If raster image and thumbs not available, use imgconvert.org.
+
+LAZY_LOAD_MAX = 12;
+LAZY_LOAD_MAX = 72;
+FIT_TO_WINDOW = false; // Code needs work.			
+
+$(document).ready(function(){
+	$("#thumbbrowsebutton").click(function () {
+		$('#g-container').hide();
+		$('#t-container').show()
+	})
+	$("#gallerybrowsebutton").click(function () {
+		$('#t-container').hide();
+		$('#g-container').show();
+		gallery("#gallery1");
+		$("#gallery1 #gallerythumbframe img").first().click(); // To trigger resize of thumb div.
+	})
+	
+	thumb("#thumb1");
+	//gallery("#gallery1"); // Causes interference with hashchange
+
+	$(window).hashchange(function(){
+		console.log('main.js: Hash has changed to ' + location.hash);
+		thumb("#thumb1");
+	});
+
+	//$("#thumbbrowsebutton").click();
+	$("#gallerybrowsebutton").click();
+	$("#gallerythumbframe").hide();
+	$("#fullframe").css("margin-left","0");
+	$("#gallery").width("300px");
+	
+	$("#skin").change(function() {
+		var newTheme =  $(this).attr('value');
+		$("#jQuery-style").attr("href",newTheme);
+		setCookie('ViVizTheme',newTheme, 365);
+		return false;
+	});
+	$('.button').mouseenter(function(){
+		$(this).removeClass('ui-state-active');
+		$(this).addClass('ui-state-hover');
+	});	
+	$('.button').mouseleave(function(){
+		$(this).removeClass('ui-state-hover');
+		$(this).addClass('ui-state-active');
+	});
+	var selectedTheme = getCookie('ViVizTheme');
+	if(selectedTheme!=null)
+	{
+		$("#jQuery-style").attr("href",selectedTheme);
+		$("#skin").val(selectedTheme);
+	}
+
+	// Thumb only
+	$('body').css('max-width', $(document).width());
+	$(window).resize(function(){
+		$('body').css('max-width', $(document).width());
+	});
+
+});
