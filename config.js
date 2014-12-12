95ce3c8f8e889ecb0a2c133fb9f5babf70eea266
Bob Weigel <rweigel@gmu.edu>
2014-12-12 13:00:18 -0500

    Adding config

---
+++ config.js
@@ -0,0 +1,18 @@
+//"CATALOGXML":"xml/catalog.xml",
+VIVIZ = {
+        "baseDirectory":"http://mag.gmu.edu/Differences/data/",
+	"defaultMode":"gallery",
+	"showThumbstrip":true,
+	"showFileName":true,
+	"showAboutText":true,
+	"showCatalog":true,
+	"showControls":true,
+	"showAttributes":true,
+	"showDownloads":true,
+	"showCatalog":true,
+	"showDropdowns":true,
+	"useAutoAttributes":true,
+	"lazyLoadMax":5,
+	"frameRate":200,
+	"useCachedImages":false
+};
